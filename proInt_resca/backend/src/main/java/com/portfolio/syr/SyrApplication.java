1dc7e7e2156451e17de426c0779310fb19cb0865
SabriResca <syresca@gmail.com>
2022-07-20 02:06:00 -0300

    cración backend

---
+++ proInt_resca/backend/src/main/java/com/portfolio/syr/SyrApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.syr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyrApplication.class, args);
+	}
+
+}
