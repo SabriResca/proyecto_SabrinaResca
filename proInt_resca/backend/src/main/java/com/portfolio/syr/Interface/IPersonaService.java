57676e589124d55dd3455cf5cfb4d6c2eedac35c
SabriResca <syresca@gmail.com>
2022-07-27 11:53:49 -0300

    backend

---
+++ proInt_resca/backend/src/main/java/com/portfolio/syr/Interface/IPersonaService.java
@@ -0,0 +1,17 @@
+
+package com.portfolio.syr.Interface;
+
+import com.portfolio.syr.Entity.Persona;
+import java.util.List;
+
+public interface IPersonaService {
+    //Traer una lista de personas
+    public List<Persona> getPersona();
+    //Guardar un objeto de tipo persona
+    public void savePersona(Persona persona);
+   //Eliminar un objeto pero lo buscamos po ID
+    public void deletePersona(Long id);
+    //Buscar una persona por id
+    public Persona findPersona(Long id);  
+}
+ 
